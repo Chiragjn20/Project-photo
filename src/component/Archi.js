9cb46c5eb92c0d924624088faf2e18748924f4f2
Chman20 <201951049@iiitvadodara.ac.in>
2021-10-26 14:33:16 +0530

    archi images updated

--- src/component/Archi.js
+++ src/component/Archi.js
@@ -10,6 +10,29 @@ import IMG3 from './Architecture/Cropped/Arc3.jpg'
 import IMG4 from './Architecture/Cropped/Arc4.jpg'
 import IMG5 from './Architecture/Cropped/Arc5.jpg'
 import IMG6 from './Architecture/Cropped/Arc6.jpg'
+import IMG7 from './Architecture/Cropped/Arc-7.jpg'
+import IMG8 from './Architecture/Cropped/Arc-8.jpg'
+import IMG9 from './Architecture/Cropped/Arc-9.jpg'
+import IMG10 from './Architecture/Cropped/Arc-10.jpg'
+import IMG11 from './Architecture/Cropped/Arc-11.jpg'
+import IMG12 from './Architecture/Cropped/Arc-12.jpg'
+import IMG13 from './Architecture/Cropped/Arc-13.jpg'
+import IMG14 from './Architecture/Cropped/Arc-14.jpg'
+import IMG15 from './Architecture/Cropped/Arc-15.jpg'
+import IMG16 from './Architecture/Cropped/Arc-16.jpg'
+import IMG17 from './Architecture/Cropped/Arc-17.jpg'
+import IMG18 from './Architecture/Cropped/Arc-18.jpg'
+import IMG19 from './Architecture/Cropped/Arc-19.jpg'
+import IMG20 from './Architecture/Cropped/Arc-20.jpg'
+import IMG21 from './Architecture/Cropped/Arc-21.jpg'
+import IMG22 from './Architecture/Cropped/Arc-22.jpg'
+import IMG23 from './Architecture/Cropped/Arc-23.jpg'
+import IMG24 from './Architecture/Cropped/Arc-24.jpg'
+import IMG25 from './Architecture/Cropped/Arc-25.jpg'
+import IMG26 from './Architecture/Cropped/Arc-26.jpg'
+
+
+
 import Instagram from './Instagram.png';
 import Email from './email.png';
 import WhatsApp from './Whatsapp.png';
@@ -44,6 +67,26 @@ function Archi() {
        <div className="div8" ><img alt="" src={IMG4}/></div>
        <div className="div8" ><img alt="" src={IMG5}/></div>
        <div className="div8" ><img alt="" src={IMG6}/></div>
+       <div className="div8" ><img alt="" src={IMG7}/></div>
+       <div className="div8" ><img alt="" src={IMG8}/></div>
+       <div className="div8" ><img alt="" src={IMG9}/></div>
+       <div className="div8" ><img alt="" src={IMG10}/></div>
+       <div className="div8" ><img alt="" src={IMG11}/></div>
+       <div className="div8" ><img alt="" src={IMG12}/></div>
+       <div className="div8" ><img alt="" src={IMG13}/></div>
+       <div className="div8" ><img alt="" src={IMG14}/></div>
+       <div className="div8" ><img alt="" src={IMG15}/></div>
+       <div className="div8" ><img alt="" src={IMG16}/></div>
+       <div className="div8" ><img alt="" src={IMG17}/></div>
+       <div className="div8" ><img alt="" src={IMG18}/></div>
+       <div className="div8" ><img alt="" src={IMG19}/></div>
+       <div className="div8" ><img alt="" src={IMG20}/></div>
+       <div className="div8" ><img alt="" src={IMG21}/></div>
+       <div className="div8" ><img alt="" src={IMG22}/></div>
+       <div className="div8" ><img alt="" src={IMG23}/></div>
+       <div className="div8" ><img alt="" src={IMG24}/></div>
+       <div className="div8" ><img alt="" src={IMG25}/></div>
+       <div className="div8" ><img alt="" src={IMG26}/></div>
               </div>
               <section className="py-5">
 
